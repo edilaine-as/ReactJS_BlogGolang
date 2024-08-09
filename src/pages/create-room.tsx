8c8fb911e078314f0d531229231dbf587954c4d6
Edilaine Alves dos Santos <91744159+edilaine-as@users.noreply.github.com>
2024-08-09 15:16:35 -0300

    Configuring React-RC 19 and working in Room

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,10 +1,15 @@
 import { useNavigate } from 'react-router-dom'
 import amaLogo from '../assets/ama-logo.svg'
+import { ArrowRight } from 'lucide-react'
 
 export function CreateRoom(){
     const navigate = useNavigate()
 
-    function handleCreateRoom(){
+    function handleCreateRoom(data: FormData){
+        const theme = data.get('theme')?.toString()
+
+        console.log(theme)
+
         navigate('/room/1234')
     }
 
@@ -17,18 +22,25 @@ export function CreateRoom(){
                 </p>
 
                 <form
-                    onSubmit={handleCreateRoom} 
+                    action={handleCreateRoom} 
                     className='flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1'
                 >
                     <input 
                         type="text" 
                         name="theme" 
                         placeholder="Nome da sala" 
-                        className='flex-1 text-sm bg-transparent mx-2 outline-none placeholder:text-zinc-500'
                         autoComplete='off'
+                        className='flex-1 text-sm bg-transparent mx-2 outline-none placeholder:text-zinc-500'
+                        required
                     />
 
-                    <button type="submit" className='bg-orange-400 px-3 py-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500'>Criar sala</button>
+                    <button 
+                        type="submit" 
+                        className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
+                    >
+                        Criar sala
+                        <ArrowRight className="size-4" />
+                    </button>
                 </form>
             </div>
         </main>
